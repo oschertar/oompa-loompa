5ad4f1e2b75ffe016fec01886fc1d7e144ad244a
oschertar <oschertar@gmail.com>
2024-08-27 19:38:01 +0200

    feat: infinite scroll implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,45 @@ import { apiService } from './services/api';
 import { ApiResponse, OompaLoompa } from './types/api';
 import Card from './components/card/Card';
 
+const THRESHOLD_INFINITE_LOADING = 0.8;
+
 function App() {
   const [page, setPage] = useState<number>(1);
   const [resultsOompaLoompa, setResultsOompaLoompa] = useState<OompaLoompa[]>([]);
+  const [total, setTotal] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(false);
 
 
   useEffect(() => {
+    setLoading(true);
     apiService.getAllOompaLoompas(page).then((res: ApiResponse) => {
       if (res.results) {
-        setResultsOompaLoompa(res.results);
+        setResultsOompaLoompa(prevResults => [...prevResults, ...res.results]);
+        setTotal(res.total);
+        setLoading(false);
       }
-
     })
-  }, [])
+  }, [page])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollTop = document.documentElement.scrollTop;
+      const windowHeight = window.innerHeight;
+
+      const scrollPosition = scrollTop + windowHeight;
+      const threshold = THRESHOLD_INFINITE_LOADING * scrollHeight;
+
+      if (scrollPosition >= threshold && !loading && page < total) {
+        setPage(prevPage => prevPage + 1);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [loading, page, total, setPage]);
+
+
 
   return (
     <>
