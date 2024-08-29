33369748c4fb1ad926fa54a2a2975a10fba041ea
oschertar <oschertar@gmail.com>
2024-08-29 20:58:21 +0200

    feat: add scroll to top on click navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,10 +3,17 @@ import "./Navbar.css"
 
 function Navbar() {
 
+    const scrollTop = () => {
+        window.scrollTo({
+            top: 100,
+            left: 100,
+            behavior: "smooth",
+        });
+    }
     return (
         <nav className="navbar">
             <div className="container">
-                <Link to="/">
+                <Link to="/" onClick={scrollTop}>
                     <h1>Oompa Loompa's Crew</h1>
                 </Link>
 
